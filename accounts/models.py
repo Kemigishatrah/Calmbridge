877e3a1b7094212ef03266d4a64bb9030911d8d2
shatrahkemi <122891423+shatrahkemi@users.noreply.github.com>
2026-01-23 09:35:55 +0300

    some edits on the models

--- accounts/models.py
+++ accounts/models.py
@@ -11,21 +11,21 @@ class User(AbstractUser):
 class TherapistProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     #professional details
-    qualifications = models.CharField(max_length=255)
-    license_number = models.CharField(max_length=50)
-    years_of_experience = models.PositiveIntegerField()
+    qualifications = models.CharField(max_length=255, blank=True)
+    license_number = models.CharField(max_length=50, blank=True)
+    years_of_experience = models.PositiveIntegerField(blank=True, null=True)
 #clinical specialization
-    specialization = models.CharField(max_length=100)
-    focus_areas = models.TextField(max_length=255)
+    specialization = models.CharField(max_length=100, blank=True)
+    focus_areas = models.TextField(max_length=255, blank=True)
     #accessibility options
-    language_proficiencies = models.CharField(max_length=100)
-    offers_teletherapy = models.BooleanField(default=False)
-    location = models.CharField(max_length=100)
+    language_proficiencies = models.CharField(max_length=100, blank=True)
+    offers_teletherapy = models.BooleanField(default=False, blank=True)
+    location = models.CharField(max_length=100, blank=True)
 #practice details
-    availability = models.CharField(max_length=100)
-    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2)
+    availability = models.CharField(max_length=100, blank=True)
+    hourly_rate = models.DecimalField(max_digits=10, decimal_places=2, blank=True, null=True)
     profile_picture = models.ImageField(upload_to='therapist_profiles/', null=True, blank=True)
-    bio = models.TextField(max_length=1000)
+    bio = models.TextField(max_length=1000, blank=True)
 
 
 
--- calmbridge/urls.py
+++ calmbridge/urls.py
@@ -16,17 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', home, name='home'), 
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('appointments/', include('appointments.urls')),
 ]
 
-# This allows Django to serve media files during development
-if settings.DEBUG:
-    urlpatterns = static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
